37e9b63912483becc99f3e32b473ef7a1c53e1cc
gahette <gahette@icloud.com>
2025-07-24 22:31:52 +0200

    Integrate Clerk authentication into layout and navbar components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
+import {SignInButton, SignedIn, SignedOut, UserButton} from "@clerk/nextjs";
 import NavItems from "./NavItems";
 
 const Navbar = () => {
-	return (
-		<nav className="navbar">
-			<Link href="/">
-				<div className="flex items-center gap-2.5 cursor-pointer">
-					<Image src="/images/logo.svg" alt="logo" width={46} height={44} />
-				</div>
-			</Link>
-			<div className="flex items-center gap-8">
-				<NavItems />
-				<p>Sign In</p>
-			</div>
-		</nav>
-	);
+    return (
+        <nav className="navbar">
+            <Link href="/">
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                    <Image src="/images/logo.svg" alt="logo" width={46} height={44}/>
+                </div>
+            </Link>
+            <div className="flex items-center gap-8">
+                <NavItems/>
+                <SignedOut>
+                    <div className="flex items-center gap-2">
+                        <SignInButton>
+                            <button className="btn-signin">Sign In</button>
+                        </SignInButton>
+                    </div>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton/>
+                </SignedIn>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
