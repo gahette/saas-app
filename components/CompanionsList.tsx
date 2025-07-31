b458727d9df9682bece084a95bc6d5f75ec9cbef
gahette <gahette@icloud.com>
2025-07-31 14:45:53 +0200

    Add user companions retrieval and enhance profile page with accordion layout

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -19,7 +19,7 @@ interface CompanionsListProps {
 const CompanionsList = ({title, companions, className}: CompanionsListProps) => {
     return (
         <article className={cn('companion-list', className)}>
-            <h2 className="font-bold text-3xl">Recent Sessions</h2>
+            <h2 className="font-bold text-3xl">{title}</h2>
 
             <Table>
                 <TableHeader>
@@ -30,8 +30,8 @@ const CompanionsList = ({title, companions, className}: CompanionsListProps) => 
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {companions?.map(({id, subject, name, topic, duration}) => (
-                        <TableRow key={id}>
+                    {companions?.map(({id, subject, name, topic, duration}, index) => (
+                        <TableRow key={index}>
                             <TableCell>
                                 <Link href={`/companions/${id}`}>
                                     <div className="flex items-center gap-2">
