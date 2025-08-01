bdef4070f28329386b9d77d0ac3287b81aa8b4a8
gahette <gahette@icloud.com>
2025-08-01 16:37:08 +0200

    Fix navigation link for My Journey and clean up code formatting

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,57 +1,57 @@
 import CompanionComponent from "@/components/CompanionComponent";
-import { getCompanion } from "@/lib/actions/companion.actions";
-import { getSubjectColor } from "@/lib/utils";
-import { currentUser } from "@clerk/nextjs/server";
+import {getCompanion} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
+import {currentUser} from "@clerk/nextjs/server";
 import Image from "next/image";
-import { redirect } from "next/navigation";
+import {redirect} from "next/navigation";
 
 interface CompanionSessionPageProps {
-  params: Promise<{ id: string }>;
+    params: Promise<{ id: string }>;
 }
 
-const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
-  const { id } = await params;
-  const companion = await getCompanion(id);
-  const user = await currentUser();
+const CompanionSession = async ({params}: CompanionSessionPageProps) => {
+    const {id} = await params;
+    const companion = await getCompanion(id);
+    const user = await currentUser();
 
-  const { name, subject, title, topic, duration } = companion;
+    const {name, subject, topic, duration} = companion;
 
-  if (!user) redirect("/sign-in");
-  if (!name) redirect("/companions");
-  return (
-    <main>
-      <article className="flex rounded-border justify-between p-6 max-md:flex-col">
-        <div className="flex items-center gap-2">
-          <div
-            className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden"
-            style={{ backgroundColor: getSubjectColor(subject) }}
-          >
-            <Image
-              src={`/icons/${subject}.svg`}
-              alt={subject}
-              width={35}
-              height={35}
+    if (!user) redirect("/sign-in");
+    if (!name) redirect("/companions");
+    return (
+        <main>
+            <article className="flex rounded-border justify-between p-6 max-md:flex-col">
+                <div className="flex items-center gap-2">
+                    <div
+                        className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden"
+                        style={{backgroundColor: getSubjectColor(subject)}}
+                    >
+                        <Image
+                            src={`/icons/${subject}.svg`}
+                            alt={subject}
+                            width={35}
+                            height={35}
+                        />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <div className="flex items-center gap-2">
+                            <p className="text-2xl font-bold">{name}</p>
+                            <div className="subject-badge max-sm:hidden">{subject}</div>
+                        </div>
+                        <p className="text-lg">{topic}</p>
+                    </div>
+                </div>
+                <div className="items-start text-2xl max-md:hidden">
+                    {duration} minutes
+                </div>
+            </article>
+            <CompanionComponent
+                {...companion}
+                companionId={id}
+                userName={user.firstName!}
+                userImage={user.imageUrl!}
             />
-          </div>
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-2">
-              <p className="text-2xl font-bold">{name}</p>
-              <div className="subject-badge max-sm:hidden">{subject}</div>
-            </div>
-            <p className="text-lg">{topic}</p>
-          </div>
-        </div>
-        <div className="items-start text-2xl max-md:hidden">
-          {duration} minutes
-        </div>
-      </article>
-      <CompanionComponent
-        {...companion}
-        companionId={id}
-        userName={user.firstName!}
-        userImage={user.imageUrl!}
-      />
-    </main>
-  );
+        </main>
+    );
 };
 export default CompanionSession;
